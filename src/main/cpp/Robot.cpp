649b53cf38080e91b2a03bd1b15aaa2804fd7799
David Baucum <dbaucum@shipcura.com>
2023-02-01 19:58:25 -0500

    Refactor code
    Various changes to the code to make it
    more DRY and readable.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,58 +1,57 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #include "Robot.h"
-
 
 void Robot::RobotInit() {}
 void Robot::RobotPeriodic() {}
-
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,0);
+  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
 }
+
 void Robot::TeleopPeriodic() {
-fr.Set(ctre::phoenix::motorcontrol::ControlMode::Follower,20);
-fl.Set(ctre::phoenix::motorcontrol::ControlMode::Follower,41);
-br.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,-((( drivecon.GetLeftY()-drivecon.GetRightX()))*drivecon.GetRightTriggerAxis()) );
-bl.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,(( drivecon.GetLeftY()+drivecon.GetRightX()))*drivecon.GetRightTriggerAxis() );
-if(drivecon.GetAButton()){
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,1);
-}
-if(drivecon.GetBButton()){
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,0);
-}
-if(drivecon.GetRightBumper()){
-  feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,.2);
-}
-else{
-  feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,0);
-}
+  frontRight.Set(ctre::phoenix::motorcontrol::ControlMode::Follower, Robot::motorControllerPort::backRight);
+  frontLeft.Set(ctre::phoenix::motorcontrol::ControlMode::Follower, Robot::motorControllerPort::backLeft);
+  backRight.Set(
+    ctre::phoenix::motorcontrol::ControlMode::PercentOutput,
+    -((driverController.GetLeftY() - driverController.GetRightX()) * driverController.GetRightTriggerAxis())
+  );
+  backLeft.Set(
+    ctre::phoenix::motorcontrol::ControlMode::PercentOutput,
+    (driverController.GetLeftY() + driverController.GetRightX()) * driverController.GetRightTriggerAxis()
+  );
+  if(driverController.GetAButton()){
+    shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 1);
+  }
+  if(driverController.GetBButton()){
+    shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+  }
+  if(driverController.GetRightBumper()){
+    feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, .2);
+  }
+  else{
+    feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+  }
 
-if(drivecon.GetLeftBumper()){
-  feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,-.2);
-}
-
+  if(driverController.GetLeftBumper()){
+    feeder.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -.2);
+  }
 }
 
 void Robot::DisabledInit() {
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,0);
+  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
 }
+
 void Robot::DisabledPeriodic() {}
-
 void Robot::TestInit() {}
+
 void Robot::TestPeriodic() {
-
-if(drivecon.GetAButtonPressed()){
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,1);
-}
-if(drivecon.GetBButtonPressed()){
-  shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput,0);
-}
-
+  if(driverController.GetAButtonPressed()) {
+    shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 1);
+  }
+  if(driverController.GetBButtonPressed()) {
+    shooter.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+  }
 }
 
 void Robot::SimulationInit() {}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
 #include "ctre/Phoenix.h"
@@ -10,17 +6,28 @@
 #include "frc/XboxController.h"
 #include <frc/TimedRobot.h>
 
-class Robot : public frc::TimedRobot {
+class Robot: public frc::TimedRobot {
 
  public:
- ctre::phoenix::motorcontrol::can::TalonSRX  fr{14};
- ctre::phoenix::motorcontrol::can::TalonSRX  br{20};
- ctre::phoenix::motorcontrol::can::TalonSRX  fl{13};
- ctre::phoenix::motorcontrol::can::TalonSRX  bl{41};
- ctre::phoenix::motorcontrol::can::TalonFX  shooter{12};/// TODO: VERIFY CAN ADDRESSES
- ctre::phoenix::motorcontrol::can::TalonFX  feeder{15};    /// TODO: VERIFY CAN ADDRESSES
- frc::XboxController drivecon{0};
- frc::XboxController opcon{1};
+  const enum motorControllerPort {
+    frontRight = 14,
+    backRight = 20,
+    frontLeft = 13,
+    backLeft = 41,
+    shooter = 12, // TODO: VERIFY CAN ADDRESSES
+    feeder = 15 // TODO: VERIFY CAN ADDRESSES
+  };
+  const enum gameControllerPort {
+    driver = 0
+  };
+
+  ctre::phoenix::motorcontrol::can::TalonSRX frontRight{motorControllerPort::frontRight};
+  ctre::phoenix::motorcontrol::can::TalonSRX backRight{motorControllerPort::backRight};
+  ctre::phoenix::motorcontrol::can::TalonSRX frontLeft{motorControllerPort::frontLeft};
+  ctre::phoenix::motorcontrol::can::TalonSRX backLeft{motorControllerPort::backLeft};
+  ctre::phoenix::motorcontrol::can::TalonFX shooter{motorControllerPort::shooter};
+  ctre::phoenix::motorcontrol::can::TalonFX feeder{motorControllerPort::feeder};
+  frc::XboxController driverController{gameControllerPort::driver};
 
   void RobotInit() override;
   void RobotPeriodic() override;
